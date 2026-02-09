509110d5921de8fc4351bb3fecaec8253cafcb20
Darlon <darloncosta050@gmail.com>
2026-02-09 14:21:56 -0300

    atividade 5

---
+++ Array/exc5/index.js
@@ -0,0 +1,16 @@
+// Criar um Novo Array (Map Manual)
+
+const numbers = [1, 2, 3, 4, 5]
+
+let multi = []
+ 
+for(let i = 0; i < numbers.length; i++) {
+    multi.push(numbers[i] * 2)
+}
+
+console.log(multi)
+
+
+
+
+
