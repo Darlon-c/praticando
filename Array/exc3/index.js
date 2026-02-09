c75866a7cf4c6540ccbe4e7634597df9bf2a0e70
Darlon <darloncosta050@gmail.com>
2026-02-09 13:17:28 -0300

    atividade 3

---
+++ Array/exc3/index.js
@@ -0,0 +1,11 @@
+// Filtrar Números Maiores que 10
+
+const values = [3, 12, 8, 23, 7, 81, 18, 1, 5, 14]
+
+for(let i = 0; i < values.length; i++) {
+
+    if(values[i] > 10) {
+        console.log(values[i])
+    }
+}
+
