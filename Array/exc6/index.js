e6e4b4bf5b51ca1cc72c63999fe8098b913e1ae1
Darlon <darloncosta050@gmail.com>
2026-02-09 18:09:39 -0300

    atividade 7

---
+++ Array/exc6/index.js
@@ -0,0 +1,11 @@
+// Inverter um Array
+
+const list = [1, 2, 3, 4, 5];
+
+let listReverse = [];
+
+for (let i = list.length - 1; i >= 0; i--) {
+    listReverse.push(list[i])
+}
+
+console.log(listReverse)
