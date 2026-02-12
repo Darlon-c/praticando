3be2f81e1f6c9dbafa91f1d745855b6a7832af18
Darlon <darloncosta050@gmail.com>
2026-02-11 22:04:41 -0300

    projeto 5

---
+++ Array/Mini-projeto5/index.js
@@ -0,0 +1,71 @@
+// Sistema de Cadastro de Produtos
+// Simula a base de um sistema de e-commerce / estoque.
+
+const productNameInput = document.getElementById("productName");
+const productPriceInput = document.getElementById("productPrice");
+const addProductBtn = document.getElementById("addProduct");
+const showInfo = document.getElementById("showInfo");
+const report = document.getElementById("report");
+
+let products = [];
+
+addProductBtn.addEventListener("click", function () {
+  const name = productNameInput.value;
+  const price = Number(productPriceInput.value);
+
+  if (name === "") {
+    alert("Informe o nome do produto");
+    return;
+  }
+  if (price <= 0) {
+    alert("Informe o valor do produto");
+    return;
+  }
+
+  const product = {
+    name: name,
+    price: price,
+  };
+
+  products.push(product);
+  productNameInput.value = "";
+  productPriceInput.value = "";
+  console.log(products);
+});
+
+report.addEventListener("click", function () {
+  if (products.length === 0) {
+    alert("Nenhum produto cadastrado!");
+    return;
+  }
+
+  let totalProducts = products.length;
+  let totalStockValue = 0;
+  let mostExpensiveProduct = products[0];
+  let leastExpensiveProduct = products[0];
+  let productsAbove100 = [];
+
+  for (let i = 0; i < products.length; i++) {
+    totalStockValue += products[i].price;
+
+    if (products[i].price > 100) {
+      productsAbove100.push(products[i].name);
+    }
+    // Mais caro
+    if (products[i].price > mostExpensiveProduct.price) {
+      mostExpensiveProduct = products[i];
+    }
+    // Mais barato
+    if (products[i].price < leastExpensiveProduct.price) {
+      leastExpensiveProduct = products[i];
+    }
+  }
+
+  showInfo.innerHTML = `
+      Total de produtos: ${totalProducts} <br>
+      Valor total do estoque: ${totalStockValue.toFixed(2)}R$ <br>
+      Produto mais caro: ${mostExpensiveProduct.name} - R$ ${mostExpensiveProduct.price.toFixed(2)} <br>
+      Produto mais barato: ${leastExpensiveProduct.name} - R$ ${leastExpensiveProduct.price.toFixed(2)} <br>
+      Produtos acima de 100 reais: ${productsAbove100.join(", ")}
+    `;
+});
