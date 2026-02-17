85ebc0d21cfec8fbe532b0c3c37d5ac0f25dcd04
Darlon <darloncosta050@gmail.com>
2026-02-17 19:32:29 -0300

    att biblioteca

--- Projetos-DOM/Biblioteca/index.js
+++ Projetos-DOM/Biblioteca/index.js
@@ -4,17 +4,25 @@ const yearInput = document.getElementById("year");
 const addBookBtn = document.getElementById("addBook");
 const bookList = document.getElementById("bookList");
 const stats = document.getElementById("stats");
+const showStats = document.getElementById("showStats");
 
 let books = [];
 let idCounter = 1;
 
+// adicionar livros
 addBookBtn.addEventListener("click", function () {
+  if (titleInput.value === "") {
+    alert("Informe o nome do livro");
+    return;
+  } else if (authorInput.value === "") {
+    alert("Informe o nome do autor");
+    return;
+  } else if (yearInput.value === "") {
+    alert("Informe o ano do livro");
+    return;
+  }
 
-    if(titleInput.value === '') {
-        alert("Informe o nome do livro")
-    }
-
-  book = {
+  const book = {
     id: idCounter,
     title: titleInput.value,
     author: authorInput.value,
@@ -22,7 +30,92 @@ addBookBtn.addEventListener("click", function () {
     available: true,
   };
 
-  books.push(book)
-  console.log(books)
+  books.push(book);
+  //mudar o valor do id
+  idCounter++;
+  console.log(books);
 
+  titleInput.value = "";
+  authorInput.value = "";
+  yearInput.value = "";
+
+  showBooks();
 });
+
+// mostrar na tela
+function showBooks() {
+  let html = "";
+
+  for (let i = 0; i < books.length; i++) {
+    html += `
+        <p>
+           Nome: ${books[i].title}  <br>
+           Autor: ${books[i].author} - (${books[i].year}) <br>
+           Status: ${books[i].available ? "Disponível" : "Emprestado"} <br>
+            <button
+                onclick="borrowBook(${books[i].id})"
+                class="px-1 py-1 bg-gray-200 rounded-xl hover:bg-gray-400 hover:font-semibold"
+                >
+                ${books[i].available ? "Emprestar" : "Devolver"}
+            </button>
+            <button
+                onclick="removeBook(${books[i].id})"
+                class="px-1 py-1 bg-gray-200 rounded-xl hover:bg-gray-400 hover:font-semibold"
+                >
+                Remover
+            </button>
+
+        </p>
+    `;
+  }
+
+  bookList.innerHTML = html;
+}
+
+// mostrar estatísticas
+
+showStats.addEventListener("click", function () {
+  let total = books.length;
+  let availableCount = 0;
+  let borrowedCount = 0;
+
+  if (books.length === 0) {
+    stats.innerHTML = "Nenhum livro cadastrado!";
+    return;
+  }
+
+  for (let i = 0; i < books.length; i++) {
+    if (books[i].available) {
+      availableCount++;
+    } else {
+      borrowedCount++;
+    }
+
+    stats.innerHTML = `
+        <p>Total de livros: ${total}</p>
+        <p>Disponíveis: ${availableCount}</p>
+        <p>Emprestados: ${borrowedCount}</p>
+    `;
+  }
+});
+
+// mudar de disponivel para emprestado e de emprestado para disponivel
+function borrowBook(id) {
+  for (let i = 0; i < books.length; i++) {
+    if (books[i].id === id) {
+      books[i].available = !books[i].available;
+    }
+  }
+  showBooks();
+}
+
+// remover livro da lista
+function removeBook(id) {
+  for (let i = 0; i < books.length; i++) {
+    if (books[i].id === id) {
+      books.splice(i, 1);
+      break;
+    }
+  }
+  showBooks();
+}
