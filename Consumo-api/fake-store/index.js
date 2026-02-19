fdd5e6e1abd04c7facaaed14f91a2b7ed02fb83b
Darlon <darloncosta050@gmail.com>
2026-02-18 22:07:06 -0300

    loja com api

---
+++ Consumo-api/fake-store/index.js
@@ -0,0 +1,9 @@
+const productList = document.getElementById("productList");
+const cartList = document.getElementById("cartList");
+const cartTotal = document.getElementById("cartTotal");
+const checkoutBtn = document.getElementById("checkoutBtn");
+
+
+
+
+
