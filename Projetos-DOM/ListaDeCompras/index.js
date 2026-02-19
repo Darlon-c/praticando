8d6ebea6a131b587868f9dc4ff42329b01b08de2
Darlon <darloncosta050@gmail.com>
2026-02-19 19:42:32 -0300

    att

--- Projetos-DOM/ListaDeCompras/index.js
+++ Projetos-DOM/ListaDeCompras/index.js
@@ -1,8 +1,43 @@
-const textInput = document.getElementById('textInput')
-const addPurchase = document.getElementById('addPurchase')
-const shoppingBasket = document.getElementById('shoppingBasket')
+const textInput = document.getElementById("textInput");
+const addPurchase = document.getElementById("addPurchase");
+const shoppingBasket = document.getElementById("shoppingBasket");
+
+let purchase = [];
+let idCount = 1;
+
+function showPurchase() {
+  for (let i = 0; i < purchase.length; i++) {
+    let buy = purchase[i];
+    const li = document.createElement("li");
+
+    li.className =
+      "flex items-center justify-between gap-2 p-3 transition-shadow bg-white border border-gray-200 rounded-lg shadow-sm hover:shadow";
+
+    li.innerHTML = `<span class="flex-1 break-words">${buy.name}</span>`;
+  
+     shoppingBasket.appendChild(li)
+  }
+
+ 
+
+}
+
+function addNewPurchase() {
+  if (textInput.value === "") {
+    alert("Por favor, informe sua compra");
+  }
+
+  const newPurchase = {
+    id: idCount,
+    name: textInput.value,
+    purchased: true,
+  };
+
+  purchase.push(newPurchase);
+  showPurchase();
+  idCount++;
+  textInput.value = "";
+}
 
 
-
-
-
+addPurchase.addEventListener("click", addNewPurchase);
