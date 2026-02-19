2f0a428084db18270deeb773cbafc9ed0bb4dc18
Darlon <darloncosta050@gmail.com>
2026-02-19 19:17:15 -0300

    lista de compras'

---
+++ Projetos-DOM/ListaDeCompras/index.js
@@ -0,0 +1,8 @@
+const textInput = document.getElementById('textInput')
+const addPurchase = document.getElementById('addPurchase')
+const shoppingBasket = document.getElementById('shoppingBasket')
+
+
+
+
+
