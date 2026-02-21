62a5ceb99d7ce6ba814ab4a3fdd400b1cf0a0b83
Darlon <darloncosta050@gmail.com>
2026-02-21 12:02:12 -0300

    exc17

---
+++ Fundamentos/Array/exc17/index.js
@@ -0,0 +1,17 @@
+// Pega apenas produtos em stock
+// Pega apenas os preços
+// Soma o valor total
+
+const products = [
+  { name: "Laptop", price: 1000, inStock: true },
+  { name: "Mouse", price: 50, inStock: true },
+  { name: "Keyboard", price: 150, inStock: false },
+  { name: "Monitor", price: 700, inStock: true },
+];
+
+const productInStock = products
+  .filter(product => product.inStock)
+  .map(product => product.price)
+  .reduce((acc, price) => acc + price, 0);
+
+console.log(productInStock);
