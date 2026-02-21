ba652c2e7a4bbed3ed54992d999c12d8bcf3eb97
Darlon <darloncosta050@gmail.com>
2026-02-21 16:10:39 -0300

    exc21

---
+++ Fundamentos/Array/exc21/index.js
@@ -0,0 +1,12 @@
+// Pega apenas números maiores que 10, Multiplica por 3 e retorna o novo array
+
+const numbers = [3, 8, 15, 22, 7, 14, 21, 2, 32, 9, 5, 4, 12];
+
+const multi = numbers
+  .filter((num) => num > 10).map(num => num * 3).reduce((acc, guard) => {
+    acc.push(guard)
+    return acc
+  }, [])
+ 
+
+console.log(multi);
