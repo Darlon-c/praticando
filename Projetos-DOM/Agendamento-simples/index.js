593c923a65d5920ed754b9d6df95e0ca9a5fb8e5
Darlon <darloncosta050@gmail.com>
2026-02-26 12:22:33 -0300

    att

--- Projetos-DOM/Agendamento-simples/index.js
+++ Projetos-DOM/Agendamento-simples/index.js
@@ -8,10 +8,12 @@ function newSchedule() {
   const textInput = document.getElementById("textName");
   const hourInput = document.getElementById("hour");
   const dateInput = document.getElementById("inputDate");
+  const carInput = document.getElementById("carInput");
 
   const name = textInput.value;
   const hour = hourInput.value;
   const date = dateInput.value;
+  const car = carInput.value;
 
   if (!name || !hour || !date) {
     alert("Por favor, preencha todos os campos!");
@@ -20,6 +22,7 @@ function newSchedule() {
 
   const scheduling = {
     id: idCount,
+    car: car,
     name: name,
     hour: hour,
     date: date,
@@ -33,11 +36,15 @@ function newSchedule() {
   textInput.value = "";
   hourInput.value = "";
   dateInput.value = "";
+  carInput.value = "";
 }
 
 function renderSchedule() {
   const showAppointments = appointments.map((scheduling) => {
-    return `<div class="border px-5 py-5"><p>Cliente: ${scheduling.name}</p><br>  <p>Data: ${scheduling.date}</p><br>  
+    return `<div class="border px-5 py-5">
+    <p>Carro: ${scheduling.car}</p><br>
+    <p>Cliente: ${scheduling.name}</p><br>  
+    <p>Data: ${scheduling.date}</p><br>  
     <p>Horario: ${scheduling.hour}</p></div>`;
   });
 
