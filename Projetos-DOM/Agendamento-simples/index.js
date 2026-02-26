cbe6087271e56af29fa23159b75d83ad519bb285
Darlon <darloncosta050@gmail.com>
2026-02-26 13:05:36 -0300

    att'

--- Projetos-DOM/Agendamento-simples/index.js
+++ Projetos-DOM/Agendamento-simples/index.js
@@ -26,6 +26,7 @@ function newSchedule() {
     name: name,
     hour: hour,
     date: date,
+    finish: false,
   };
 
   appointments.push(scheduling);
@@ -36,19 +37,49 @@ function newSchedule() {
   textInput.value = "";
   hourInput.value = "";
   dateInput.value = "";
-  carInput.value = "";
 }
 
 function renderSchedule() {
   const showAppointments = appointments.map((scheduling) => {
-    return `<div class="border px-5 py-5">
+    const bgColor = scheduling.finish ? "bg-green-200" : "bg-white";
+
+    return `<div class="border px-5 py-5 ">
     <p>Carro: ${scheduling.car}</p><br>
     <p>Cliente: ${scheduling.name}</p><br>  
     <p>Data: ${scheduling.date}</p><br>  
-    <p>Horario: ${scheduling.hour}</p></div>`;
+    <p>Horario: ${scheduling.hour}</p>
+    <button onclick="testDone(${scheduling.id})" class="px-3 border ${bgColor} rounded-md hover:bg-slate-100">Test feito</button>
+    <button onclick="removeAppointments(${scheduling.id})" class="px-3 border rounded-md hover:bg-red-500">X</button>
+    </div>`;
   });
 
   list.innerHTML = showAppointments.join("");
+}
+
+function removeAppointments(id) {
+  appointments = appointments.filter((scheduling) => {
+    return scheduling.id !== id;
+  });
+
+  renderSchedule();
+  alert("Agendamento removido");
+}
+
+function testDone(id) {
+  // Encontra o agendamento pelo ID
+  const schedulings = appointments.find((item) => item.id === id);
+
+  if (schedulings) {
+    schedulings.finish = !schedulings.finish;
+
+    if (schedulings.finish) {
+      alert(`Agendamento #${id} marcado como concluído!`);
+    } else {
+      alert(`Agendamento #${id} reaberto!`);
+    }
+  }
+
+  renderSchedule();
 }
 
 toSchedule.addEventListener("click", newSchedule);
