4c290c1562415fec3312c7315f1a44472b5c4739
Darlon <darloncosta050@gmail.com>
2026-02-27 11:15:41 -0300

    pokeApi Att

--- Consumo-api/pokeAPI/index.js
+++ Consumo-api/pokeAPI/index.js
@@ -1,5 +1,28 @@
+const poke = document.getElementById("pokemons");
+const pokeInput = document.getElementById("pokeInput");
+const searchPoke = document.getElementById("searchPoke");
 
+async function getPokemon() {
+  const name = pokeInput.value.toLowerCase();
 
+  if (!name) {
+    poke.innerHTML = "Digite um nome";
+    return;
+  }
 
+  try {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+    const data = await response.json();
 
+    poke.innerHTML = `
+    <div class="flex justify-center flex-col">
+        <h2 class="">Nome: <span class="font-mono text-indigo-600">${data.name}</span></h2>
+        <p>${data.id}</p>
+        <img class="max-w-40" src="${data.sprites.front_shiny}">
+        <p>${data.item}</p>
+    </div>
+    `;
+  } catch (err) {}
+}
 
+searchPoke.addEventListener("click", getPokemon);
