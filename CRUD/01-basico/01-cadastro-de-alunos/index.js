f1db3d35a4271f54865342316e391cd2d077138d
Darlon <darloncosta050@gmail.com>
2026-02-27 17:42:58 -0300

    atividade finalizada

--- CRUD/01-basico/01-cadastro-de-alunos/index.js
+++ CRUD/01-basico/01-cadastro-de-alunos/index.js
@@ -2,8 +2,11 @@ const studentName = document.getElementById("studentName");
 const studentAge = document.getElementById("studentAge");
 const registerStudent = document.getElementById("registerStudent");
 const result = document.getElementById("result");
+const totalStudent = document.getElementById("totalStudent");
+
 
 let listOfStudents = [];
+let registration = 1;
 
 function addStudent() {
   if (studentName.value === "") {
@@ -15,20 +18,45 @@ function addStudent() {
   }
 
   const student = {
+    id: registration,
     name: studentName.value,
     age: Number(studentAge.value),
   };
 
   listOfStudents.push(student);
-
+  registration++;
+  renderStudents();
+  console.log(listOfStudents);
   studentAge.value = "";
   studentName.value = "";
 }
 
 function renderStudents() {
-  
+  const showStudents = listOfStudents.map((students) => {
+    return `
+      <div>
+        <p>Nome: ${students.name} | Idade: ${students.age} | Matrícula: ${students.id}</p>
+        <button onclick="removeStudent(${students.id})">Remover</button>
+      </div>
+    `;
+  });
+
+  result.innerHTML = showStudents.join("");
+  totalStudent.innerHTML = `Total de alunos: ${listOfStudents.length}`
 }
 
+function removeStudent(id) {
+  listOfStudents = listOfStudents.filter((student) => {
+    return student.id !== id;
+  });
+
+  renderStudents();
+  totalStudent.innerHTML = totalStudents
+}
+
+ const totalStudents = listOfStudents.reduce((acc, students) =>{
+      return acc + students
+},0)
 
 
 registerStudent.addEventListener("click", addStudent);
