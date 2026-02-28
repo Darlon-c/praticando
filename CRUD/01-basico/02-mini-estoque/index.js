a40902ecfc30daddae951cd0db08330c316cc521
Darlon <darloncosta050@gmail.com>
2026-02-28 15:23:31 -0300

    att

--- CRUD/01-basico/02-mini-estoque/index.js
+++ CRUD/01-basico/02-mini-estoque/index.js
@@ -5,34 +5,71 @@ const productQuantity = document.getElementById("productQuantity");
 const result = document.getElementById("result");
 const totalPriceProducts = document.getElementById("totalPriceProducts");
 
-let listProducts = []
-let idCount = 1
+let listProducts = [];
+let idCount = 1;
 
 function createProduct() {
-    if(productName.value === '') {
-        alert('Informe o nome do produto!')
-        return
-    } else if(productPrice.value === '')  {
-        alert('Informe o valor do produto!')
-        return
-    } else if(productQuantity.value === '') {
-        alert('Informe a quantidade do produto!')
-        return
-    }
+  if (productName.value === "") {
+    alert("Informe o nome do produto!");
+    return;
+  } else if (productPrice.value === "") {
+    alert("Informe o valor do produto!");
+    return;
+  } else if (productQuantity.value === "") {
+    alert("Informe a quantidade do produto!");
+    return;
+  }
 
-    const product = {
-        id: idCount,
-        name: productName.value,
-        price: productPrice.value,
-        quantity: productQuantity.value
-    }
+  const product = {
+    id: idCount,
+    name: productName.value,
+    price: productPrice.value,
+    quantity: productQuantity.value,
+  };
 
-    listProducts.push(product)
-    productName.value = ''
-    productPrice.value = ''
-    productQuantity.value = ''
+  listProducts.push(product);
+  renderProducts();
 
+  idCount++;
+  productName.value = "";
+  productPrice.value = "";
+  productQuantity.value = "";
+
+  console.log(listProducts);
 }
 
+function renderProducts() {
+  const shelf = listProducts.map((product) => {
+    return `
+            <div class="flex">
+                <p>Nome: ${product.name} | 
+                    Preço: R$ ${product.price} |
+                    Quantidade: ${product.quantity} | 
+                    Valor total: R$ ${product.price * product.quantity} 
+                </p>
+                <button onclick="removeProduct(${product.id})">Remover</button>
+            </div>
+        `;
+  });
 
-registerProduct.addEventListener('click', createProduct)
+  result.innerHTML = shelf.join("");
+  showTotalValue();
+}
+
+function removeProduct(id) {
+  listProducts = listProducts.filter((product) => {
+    return product.id !== id;
+  });
+
+  renderProducts();
+}
+
+function showTotalValue() {
+  const totalPrice = listProducts.reduce((acc, product) => {
+    return (acc += product.price * product.quantity);
+  }, 0);
+
+  totalPriceProducts.innerHTML = `Valor total do estoque: ${totalPrice}`;
+}
+
+registerProduct.addEventListener("click", createProduct);
