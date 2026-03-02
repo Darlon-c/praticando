0f4eab7c8f33152ab23106e29c065a3cbdf4f2ca
Darlon <darloncosta050@gmail.com>
2026-03-02 11:52:01 -0300

    att

--- CRUD/01-basico/03-tarefas-com-prioridade/index.js
+++ CRUD/01-basico/03-tarefas-com-prioridade/index.js
@@ -22,8 +22,37 @@ function addTask() {
   taskList.push(task);
   idCount++;
 
+  rendertask();
   taskName.value = "";
   console.log(taskList);
+}
+
+function rendertask() {
+  const showTask = taskList.map((task) => {
+    return `
+            <div class="flex">
+                <p>${task.title} | 
+                    Prioridade: ${task.priority} 
+                   
+                </p>
+                <button onclick="changePriority(${task.id})">Mudar prioridade</button>
+                <button onclick="editTitle(${task.id})">Editar título</button>
+                <button onclick="removeTask(${task.id})">Remover</button>
+                
+            </div>
+        `;
+  });
+
+  resultTasks.innerHTML = showTask.join("");
+}
+
+
+function removeTask(id) {
+    taskList = taskList.filter((task) => {
+        return task.id !== id
+    })
+
+    rendertask()
 }
 
 btnTask.addEventListener("click", addTask);
