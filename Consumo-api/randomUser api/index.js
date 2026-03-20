c76e85eca2fc2b76b2ebfdaeae101c1c0d432c0e
Darlon <darloncosta050@gmail.com>
2026-03-20 12:49:15 -0300

    ajustes no codigo

--- Consumo-api/randomUser api/index.js
+++ Consumo-api/randomUser api/index.js
@@ -4,7 +4,7 @@ const favoriteUser = document.getElementById("favoriteUser");
 const userDiv = document.getElementById("userDiv");
 
 let favorite = [];
-let currentUser = null
+let currentUser = null;
 
 async function getUser() {
   try {
@@ -12,20 +12,20 @@ async function getUser() {
     const data = await response.json();
 
     const user = data.results[0];
-    currentUser = user
+    currentUser = user;
 
-    console.log(user);
+    const date = new Date(user.dob.date).toLocaleDateString("pt-BR");
 
     result.innerHTML = `
         <div>
+            <img src="${user.picture.large}">
             <h3>Nome completo: ${user.name.first} ${user.name.last}</h3>
             <h3>Idade: ${user.dob.age}</h3>
-            <h3>Data de nascimento: ${user.dob.date}</h3>
+            <h3>Data de nascimento: ${date}</h3>
             <h3>Celular: ${user.cell}</h3>
             <h3>CPF: ${user.id.value}</h3>
             <h3>Endereço: ${user.location.street.name} Nº${user.location.street.number} ${user.location.city}</h3>
-            <img src="${user.picture.large}">
-            <button class="border bg-emerald-300" onclick="bestUsers()">Favoritar</button<
+            <button class="border bg-emerald-300" onclick="bestUsers()">Favoritar</button>
         </div>
     `;
   } catch (error) {
@@ -34,28 +34,27 @@ async function getUser() {
 }
 
 function bestUsers() {
-    favorite.push(currentUser)
-    console.log(favorite)
+  favorite.push(currentUser);
+  console.log(favorite);
 }
 
 function showFavoriteUser() {
-    const render = favorite.map((user) => {
-        return `
+  const render = favorite.map((user) => {
+    return `
         <div>
+            <img src="${user.picture.large}">
             <h3>Nome completo: ${user.name.first} ${user.name.last}</h3>
             <h3>Idade: ${user.dob.age}</h3>
             <h3>Data de nascimento: ${user.dob.date}</h3>
             <h3>Celular: ${user.cell}</h3>
             <h3>CPF: ${user.id.value}</h3>
             <h3>Endereço: ${user.location.street.name} Nº${user.location.street.number} ${user.location.city}</h3>
-            <img src="${user.picture.large}">
-           
         </div>
     `;
-    })
+  });
 
-    userDiv.innerHTML = render.join("")
+  userDiv.innerHTML = render.join("");
 }
 
 btn.addEventListener("click", getUser);
-favoriteUser.addEventListener("click", showFavoriteUser)
+favoriteUser.addEventListener("click", showFavoriteUser);
