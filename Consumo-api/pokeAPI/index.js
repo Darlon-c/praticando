2c784730f260c20d69f2befdad514fe432bac2a0
Darlon <darloncosta050@gmail.com>
2026-03-23 10:22:42 -0300

    function add favorites pokemons

--- Consumo-api/pokeAPI/index.js
+++ Consumo-api/pokeAPI/index.js
@@ -1,8 +1,11 @@
 const poke = document.getElementById("pokemons");
 const pokeInput = document.getElementById("pokeInput");
 const searchPoke = document.getElementById("searchPoke");
+const btnFavorites = document.getElementById("btnFavorites");
+const renderFavorites = document.getElementById("renderFavorites");
 
 let favoritePokemons = [];
+let currentPokemon = null;
 
 async function getPokemon() {
   const name = pokeInput.value.toLowerCase();
@@ -16,6 +19,8 @@ async function getPokemon() {
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
     const data = await response.json();
 
+    const pokemon = data;
+    currentPokemon = pokemon;
     const types = data.types.map((t) => t.type.name).join(" / ");
 
     poke.innerHTML = `
@@ -23,14 +28,7 @@ async function getPokemon() {
         <h2 class=""><strong>Nome:</strong> <span class="font-mono text-indigo-600">${data.name}</span></h2>
         <img src="${data.sprites.front_default}">
         <p><strong>Tipo:</strong> <span class="font-mono text-indigo-600">${types}</span></p>
-        <div>
-          <p>
-            Altura: ${data.height / 10}
-          </p>
-          <p>
-            Altura: ${data.weigth / 10}
-          </p>
-        </div>
+        <button class="border bg-indigo-600" onclick="addFavorites()">Favoritar</button>
     </div>
     `;
     console.log(data);
@@ -39,5 +37,33 @@ async function getPokemon() {
   }
 }
 
+function showFavorites() {
+  const render = favoritePokemons.map((pokemon) => {
+    return `
+    <div class="flex justify-center flex-col">
+        <h2 class=""><strong>Nome:</strong> <span class="font-mono text-indigo-600">${pokemon.name}</span></h2>
+        <p><strong>Tipo:</strong> ${pokemon.types.map((t) => t.type.name).join(" / ")}</p>
+        <img src="${pokemon.sprites.front_default}">
+    </div>`;
+  });
+
+  renderFavorites.innerHTML = render.join("");
+}
+
+function addFavorites() {
+  favoritePokemons.push(currentPokemon);
+
+  console.log(favoritePokemons);
+}
+
 pokeInput.addEventListener("keydown", (e) => {});
 searchPoke.addEventListener("click", getPokemon);
+btnFavorites.addEventListener("click", showFavorites);
+// <div>
+//           <p>
+//             Altura: ${data.height / 10}
+//           </p>
+//           <p>
+//             Altura: ${data.weigth / 10}
+//           </p>
+//         </div>
