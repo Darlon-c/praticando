52191b4f4b5f76542a14cdfe2af2314b6f454372
Darlon <darloncosta050@gmail.com>
2026-03-24 09:12:25 -0300

    att

---
+++ Consumo-api/Auth/src/index.js
@@ -0,0 +1,13 @@
+const firstName = document.getElementById('firstName')
+const emailRegister = document.getElementById('emailRegister')
+const passwordRegister = document.getElementById('passwordRegister')
+const passwordConfirm = document.getElementById('passwordConfirm')
+const btnRegister = document.getElementById('btnRegister')
+const emailLogin = document.getElementById('emailLogin')
+const passwordLogin = document.getElementById('passwordLogin')
+const btnLogin = document.getElementById('btnLogin')
+
+
+
+
+
